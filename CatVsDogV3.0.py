696ac9a7d7032efe7565f75475bcf0af5fe8278f
M-C-AD <30936333+M-C-AD@users.noreply.github.com>
2021-09-09 21:27:58 +0100

    Update CatVsDogV3.0.py

--- CatVsDogV3.0.py
+++ CatVsDogV3.0.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torchvision
 import torch.nn as nn
@@ -7,14 +8,29 @@ from torch.utils.data import DataLoader
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
+data_dir = './data/Cat_Dog_data'
+print(data_dir)
+
 #Set Device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Set Hyperparameters
-#input_size = 28
+#num_workers = 4
+batch_size = 64
+# pin_memory = True
+# load_model = True
+# save_model = True
+# weight_decay = 0.0001
+learning_rate = 0.0001
 num_layers = 2
 num_classes = 10
-learning_rate = 0.001
-batch_size = 64
 num_epochs = 2
+
+
+
+
+
+
+
+
 
