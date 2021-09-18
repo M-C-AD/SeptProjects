07571085366eaa6e8a0b814f64658e78aa3a9537
M-C-AD <30936333+M-C-AD@users.noreply.github.com>
2021-09-18 14:07:22 +0100

    Update CatVsDogV3.0.py

--- CatVsDogV3.0.py
+++ CatVsDogV3.0.py
@@ -97,6 +97,15 @@ class BaseImageClassificationModel(nn.Module):
         epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
         return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
 
+    def epoch_end(self, epoch, result):
+        print("Epoch[{}], train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}"
+              .format(epoch, result['train_loss'], result['val_loss'], result['val_acc']))
+
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
 
 class CDNet(nn.Module):
     def __init__(self):
