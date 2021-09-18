de3f11deee78f67c43600a8081cd274206bd8f8a
M-C-AD <30936333+M-C-AD@users.noreply.github.com>
2021-09-18 19:36:46 +0100

    Update CatVsDogV3.0.py

--- CatVsDogV3.0.py
+++ CatVsDogV3.0.py
@@ -146,7 +146,7 @@ for images, labels in train_Dloader:
 
 criterian = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-print(model)
+print(model) #*************************************
 
 
 # Device Management
@@ -179,8 +179,37 @@ class DeviceDataLoader():
         """Number of batches"""
         return len(self.dl)
 
+# print(device) # *************************
+train_Dloader = DeviceDataLoader(train_Dloader, device)
+to_device(model, device)
 
 
+@torch.no_grad()
+def evaluate(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    history = []
+    optimizer = opt_func(model.parameters(), lr)
+    for epoch in range(epochs):
+        # Training Phase
+        model.train()
+        train_losses = []
+        for batch in train_loader:
+            loss = model.training_setp(batch)
+            train_losses.append(loss)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        # Validation phase
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
 
 
 
